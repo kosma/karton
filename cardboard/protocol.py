0af1dd33647185b8586d1e0cd9dba013765aa396
Kosma Moczek <kosma@kosma.pl>
2013-03-04 18:51:41 +0100

    add cardboard/protocol.py + tests

---
+++ cardboard/protocol.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+"""
+Minimal Redis protocol implementation.
+
+Reply translation table (Redis <- Python):
+
+    Status Reply <- true, Status
+    Error Reply <- Exception, Error
+    Integer Reply <- int
+    Bulk Reply <- str
+    NULL Bulk Reply <- None
+    Multi Bulk Reply <- list
+
+Protocol parsing is handled by hiredis at the moment.
+"""
+
+class Status(object):
+
+    def __init__(self, message):
+        self.message = message
+
+    def __repr__(self):
+        return '<Status reply +%s>' % self.message
+
+
+class Error(object):
+
+    def __init__(self, message):
+        self.message = message
+
+    def __repr__(self):
+        return '<Error reply -%s>' % self.message
+
+
+OK = Status('OK')
+
+
+def python_to_redis(response):
+    if response is True:
+        return '+OK\r\n'
+    elif response is False:
+        return '-ERR\r\n'
+    elif isinstance(response, Status):
+        return '+%s\r\n' % response.message
+    elif isinstance(response, Error):
+        return '-%s\r\n' % response.message
+    elif isinstance(response, Exception):
+        return '-ERR %s\r\n' % repr(response)
+    elif isinstance(response, (int, long)):
+        return ':%d\r\n' % response
+    elif isinstance(response, str):
+        return '$%d\r\n%s\r\n' % (len(response), response)
+    elif response is None:
+        return '$-1\r\n'
+    elif isinstance(response, list):
+        return ('*%d\r\n' % len(response)) + ''.join(map(python_to_redis, response))
+    else:
+        raise ValueError("don't know how to handle %s" % repr(response))
---
+++ tests/test_protocol.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+from cardboard.protocol import python_to_redis, Status, Error
+
+
+def test_python_to_redis():
+    # http://redis.io/topics/protocol
+
+    # Status reply
+    assert python_to_redis(True) == '+OK\r\n'
+    assert python_to_redis(Status('PONG')) == '+PONG\r\n'
+    assert python_to_redis(Status('GOOD')) == '+GOOD\r\n'
+
+    # Error reply
+    assert python_to_redis(False) == '-ERR\r\n'
+    assert python_to_redis(Error('WRONGTYPE')) == '-WRONGTYPE\r\n'
+    assert python_to_redis(Error("ERR I'm a teapot")) == "-ERR I'm a teapot\r\n"
+
+    # Integer reply
+    assert python_to_redis(0) == ':0\r\n'
+    assert python_to_redis(42) == ':42\r\n'
+    assert python_to_redis(42L) == ':42\r\n'
+    assert python_to_redis(36893488147419103232) == ':36893488147419103232\r\n'
+    assert python_to_redis(36893488147419103232L) == ':36893488147419103232\r\n'
+
+    # Bulk reply
+    assert python_to_redis('') == '$0\r\n\r\n'
+    assert python_to_redis('foo') == '$3\r\nfoo\r\n'
+    assert python_to_redis('foo\0bar\r\n') == '$9\r\nfoo\0bar\r\n\r\n'
+
+    # NULL bulk reply
+    assert python_to_redis(None) == '$-1\r\n'
+
+    # Multi-bulk reply
+    assert python_to_redis(['foo', 'ba\nr', 'xyzzy']) == '*3\r\n$3\r\nfoo\r\n$4\r\nba\nr\r\n$5\r\nxyzzy\r\n'
+    assert python_to_redis([True, False, 42, 'wat']) == '*4\r\n+OK\r\n-ERR\r\n:42\r\n$3\r\nwat\r\n'
+    assert python_to_redis([True, ['foo', 'bar'], True]) == '*3\r\n+OK\r\n*2\r\n$3\r\nfoo\r\n$3\r\nbar\r\n+OK\r\n'
+
+    # Not supported: NULL multi-bulk reply (*-1)
